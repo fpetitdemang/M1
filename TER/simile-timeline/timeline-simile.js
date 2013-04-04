eaa5cbcf28e5552b45b04f04a62ad27bd4a02a76
fpetitdemang <fpetitdemang@n18.etu.info-ufr.univ-montp2.fr>
2013-04-04 19:20:11 +0200

    toujours en guerre avec la frise chronologique

--- TER/simile-timeline/timeline-simile.js
+++ TER/simile-timeline/timeline-simile.js
@@ -27,13 +27,39 @@
 
 
 function onLoad() {
-
+	var eventSource = new Timeline.DefaultEventSource();
 	//modification theme
+	
+	var maFonction = function(){ 
+	console.log(this);
+	return "images/red-circle.png";
+	};
+	
+	
 	var theme = Timeline.ClassicTheme.create(); // create the theme
-        //theme.event.instant.icon = function(){ return "images/red-circle.png"; };//avec un fonction anomyme pour charger dynamquement le chmein de l'image
+        theme.event.instant.icon = maFonction();//avec un fonction anomyme pour charger dynamquement le chmein de l'image
 	//theme.event.instant.icon = "images/red-circle.png";
-	console.log(function(){ return "images/red-circle.png";});
-   var eventSource = new Timeline.DefaultEventSource();
+	
+	
+	var dateEvent = new Date();
+      	dateEvent.setTime(dateEvent.getTime() + ((Math.floor(Math.random()*41) - 20) * 24 * 60 * 60 * 1000));
+	
+	var evt = new Timeline.DefaultEventSource.Event(
+         dateEvent, //start
+         dateEvent, //end
+         dateEvent, //latestStart
+         dateEvent, //earliestEnd
+         true, //instant
+         "Event ", //text
+         "Description for Event ", //description
+         "images/red-circle.png"
+      );
+      
+      eventSource.add(evt);
+      eventSource.add(evt);
+	
+	//console.log(maFonction());
+   
    var bandInfos = [
      Timeline.createBandInfo({
          eventSource: eventSource,
@@ -57,12 +83,76 @@ function onLoad() {
    
    tl = Timeline.create(document.getElementById("my-timeline"), bandInfos);
    Timeline.loadXML("example1.xml", function(xml, url) { eventSource.loadXML(xml, url); });
+  
+   console.log(eventSource);
    
+
    //dynTest();
+
    
    
  }
+ /*
  
+ function onLoad() {
+
+  var eventSource = new Timeline.DefaultEventSource();
+  //Generate 50 random events up to 20 days in the past or the future
+  for(var i=0;i<50;i++) {
+      var dateEvent = new Date();
+      dateEvent.setTime(dateEvent.getTime()+((Math.floor(Math.random()*41) - 20) * 24 * 60 * 60 * 1000));
+      var evt = new Timeline.DefaultEventSource.Event(
+         dateEvent, //start
+         dateEvent, //end
+         dateEvent, //latestStart
+         dateEvent, //earliestEnd
+         true, //instant
+         "Event " + i, //text
+         "Description for Event " + i, //description
+        "images/red-circle.png",
+         "images/red-circle.png",
+                "images/red-circle.png",
+                "red",
+                "blue"
+      );
+      eventSource.add(evt);
+      console.log(dateEvent);
+  }
+  //create the timeline
+  var bandInfos = [
+    Timeline.createBandInfo({
+        trackGap:       0.2,
+        width:          "70%",
+        intervalUnit:   Timeline.DateTime.DAY,
+        intervalPixels: 50,
+        eventSource: eventSource
+    }),
+    Timeline.createBandInfo({
+        showEventText:  false,
+        trackHeight:    0.5,
+        trackGap:       0.2,
+        width:          "30%",
+        intervalUnit:   Timeline.DateTime.MONTH,
+        intervalPixels: 150,
+        eventSource: eventSource
+    })
+  ];
+  bandInfos[1].syncWith = 0;
+  bandInfos[1].highlight = true;
+  tl = Timeline.create(document.getElementById("my-timeline"), bandInfos);
+
+}
+
+var resizeTimerID = null;
+function onResize() {
+    if (resizeTimerID == null) {
+        resizeTimerID = window.setTimeout(function() {
+            resizeTimerID = null;
+            tl.layout();
+        }, 500);
+    }
+}
+ */
  
 
 
