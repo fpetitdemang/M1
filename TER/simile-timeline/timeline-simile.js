a8a2deb2b7653cb9642d3f76b64f9478a980016c
fpetitdemang <fpetitdemang@n18.etu.info-ufr.univ-montp2.fr>
2013-04-04 16:49:55 +0200

    maj

--- TER/simile-timeline/timeline-simile.js
+++ TER/simile-timeline/timeline-simile.js
@@ -1,107 +1,69 @@
+ function dynTest(){
+    document.getElementsByClassName = function(cl, element)
+{
+    var retnode = [];
+    var myclass = new RegExp('\\b'+cl+'\\b');
+    var elem = element.getElementsByTagName('*');
+    for (var i = 0; i < elem.length; i++)
+    {
+        var classes = elem[i].className;
+        var testRegExp = new RegExp("neutre");
+        //if (testRegExp.test(classes)) console.log(classes);
+        console.log(classes + " : " +document.compareDocumentPosition(elem[i]));
+        if (myclass.test(classes))
+            retnode.push(elem[i]);
+         
+    }
+    return retnode;
+};
+
+   var tabItem = document.getElementsByClassName("positif", document);
+   console.log(tabItem);
+}
+
+
+
+
+
+
 function onLoad() {
+
+	//modification theme
+	var theme = Timeline.ClassicTheme.create(); // create the theme
+        //theme.event.instant.icon = function(){ return "images/red-circle.png"; };//avec un fonction anomyme pour charger dynamquement le chmein de l'image
+	//theme.event.instant.icon = "images/red-circle.png";
+	console.log(function(){ return "images/red-circle.png";});
    var eventSource = new Timeline.DefaultEventSource();
    var bandInfos = [
      Timeline.createBandInfo({
-         eventSource:    eventSource,
-         date:           "Jun 28 2006 00:00:00 GMT",
-         width:          "25%", 
-         intervalUnit:   Timeline.DateTime.MONTH, 
+         eventSource: eventSource,
+         date: "Jun 28 2006 00:00:00 GMT",
+         width: "70%",
+         theme: theme,
+         intervalUnit: Timeline.DateTime.MONTH,
          intervalPixels: 100,
      }),
      Timeline.createBandInfo({
-        overview:       true,
-         eventSource:    eventSource,
-         date:           "Jun 28 2006 00:00:00 GMT",
-         width:          "25%", 
-         intervalUnit:   Timeline.DateTime.MONTH, 
+        overview: true,
+         eventSource: eventSource,
+         date: "Jun 28 2006 00:00:00 GMT",
+         width: "30%",
+         intervalUnit: Timeline.DateTime.MONTH,
          intervalPixels: 100
      }),
-     Timeline.createBandInfo({
-        overview:       true,
-         eventSource:    eventSource,
-         date:           "Jun 28 2006 00:00:00 GMT",
-         width:          "25%", 
-         intervalUnit:   Timeline.DateTime.MONTH, 
-         intervalPixels: 100
-     }),
-     Timeline.createBandInfo({
-        overview:       true,
-         eventSource:    eventSource,
-         date:           "Jun 28 2006 00:00:00 GMT",
-         width:          "25%", 
-         intervalUnit:   Timeline.DateTime.MONTH, 
-         intervalPixels: 100
-     })
    ];
    bandInfos[0].syncWith = 1;
-   bandInfos[1].syncWith = 2;	
-   bandInfos[2].syncWith = 3;
-   bandInfos[1].highlight = true;
+ 
    
    tl = Timeline.create(document.getElementById("my-timeline"), bandInfos);
    Timeline.loadXML("example1.xml", function(xml, url) { eventSource.loadXML(xml, url); });
+   
+   //dynTest();
+   
+   
  }
  
- /*
-function onLoad() {
-var eventSource = new Timeline.DefaultEventSource();
-  var bandInfos = [
-         Timeline.createBandInfo({
-            date:           "Jun 28 2006 00:00:00 GMT",
-            width:          "35%", 
-            intervalUnit:   Timeline.DateTime.MONTH, 
-            intervalPixels: 100,
-            eventSource:    eventSource,
-            zoomIndex:      10,
-            zoomSteps:      new Array(
-              {pixelsPerInterval: 280,  unit: Timeline.DateTime.HOUR},
-              {pixelsPerInterval: 140,  unit: Timeline.DateTime.HOUR},
-              {pixelsPerInterval:  70,  unit: Timeline.DateTime.HOUR},
-              {pixelsPerInterval:  35,  unit: Timeline.DateTime.HOUR},
-              {pixelsPerInterval: 400,  unit: Timeline.DateTime.DAY},
-              {pixelsPerInterval: 200,  unit: Timeline.DateTime.DAY},
-              {pixelsPerInterval: 100,  unit: Timeline.DateTime.DAY},
-              {pixelsPerInterval:  50,  unit: Timeline.DateTime.DAY},
-              {pixelsPerInterval: 400,  unit: Timeline.DateTime.MONTH},
-              {pixelsPerInterval: 200,  unit: Timeline.DateTime.MONTH},
-              {pixelsPerInterval: 100,  unit: Timeline.DateTime.MONTH} // DEFAULT zoomIndex
-            )
-        }),
-	
-		Timeline.createBandInfo({
-            date:           "Jun 28 2006 00:00:00 GMT",
-            width:          "35%", 
-            intervalUnit:   Timeline.DateTime.MONTH, 
-            intervalPixels: 100,
-            showEventText:  false, 
-            trackHeight:    0.5,
-            trackGap:       0.2,
-            eventSource:    eventSource,
-            overview:       true
-        }),
-	
+ 
 
-	
-	
-        Timeline.createBandInfo({
-            date:           "Jun 28 2006 00:00:00 GMT",
-            width:          "30%", 
-            intervalUnit:   Timeline.DateTime.YEAR, 
-            intervalPixels: 200,
-            showEventText:  false, 
-            trackHeight:    0.5,
-            trackGap:       0.2,
-            eventSource:    eventSource,
-            overview:       true
-        })
-	
 
-  ];
-  bandInfos[2].syncWith = 0;
-  bandInfos[1].syncWith = 2;
-  bandInfos[1].highlight = true;
-  
-  tl = Timeline.create(document.getElementById("my-timeline"), bandInfos);
-  Timeline.loadXML("example1.xml", function(xml, url) { eventSource.loadXML(xml, url); });
-  
-}*/
+
